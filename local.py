0cdaaa36e55dee76dbea7039544594ac69b3eff9
SkyLeo00 <dlxogh0630@gmail.com>
2024-07-26 13:26:38 +0900

    7.16_13:25

--- local.py
+++ local.py
@@ -21,22 +21,52 @@ def image_to_base64(img):
     return img_b64
 
 
+#state of positions
+isfall = False
+issitting = False
+iswalking = False
+isstanding = False
+isjump = False
+
 # Firebase update function
 def update_firebase(ref, labels):
+    global isfall, issitting, iswalking, isstanding, isjump
+
     for label in labels:
         label_name = label.split(": ")[0]
         pos = label.split(": ")[1]
         if float(pos) > 0.8:
             if label_name == "fall":
                 ref.update({"fall": True})
+                isfall = True
             elif label_name == "jump":
                 ref.update({"jump": True})
+                isjump = True
             elif label_name == "sitting":
                 ref.update({"sitting": True})
+                issitting = True
             elif label_name == "standing":
                 ref.update({"standing": True})
+                isstanding = True
             elif label_name == "walking":
                 ref.update({"walking": True})
+                iswalking = True
+        else:
+            if(isfall):
+                ref.update({"fall": False })
+                isfall = False
+            if(isjump):
+                ref.update({"jump": False })
+                isjump = False
+            if(isstanding):
+                ref.update({"standing": False })
+                isstanding = False
+            if(issitting):
+                ref.update({"sitting": False })
+                issitting = False
+            if(iswalking):
+                ref.update({"walking": False })
+                iswalking = False
 
 
 # Function to start the video stream and perform object detection
@@ -45,7 +75,7 @@ def start_video_and_detect():
     #######firebase Setting#################
     # Environment Setting for using firebase
     cred = credentials.Certificate(
-        "/Users/sangwon_back/Chrome_download/IoT_Project/json/silvercare-84496-firebase-adminsdk-tksu6-bac3439fd8.json"
+        "./json/silvercare-84496-firebase-adminsdk-tksu6-bac3439fd8.json"
     )
     # cred = credentials.Certificate('/Users/sangwon_back/Chrome_download/IoT_Project/local_execution/silvercare-84496-firebase-adminsdk-tksu6-bac3439fd8.json')
 
@@ -69,16 +99,16 @@ def start_video_and_detect():
     nms_threshold = 0.45
 
     # Load the YOLOv8 model
-    model = YOLO("/Users/sangwon_back/Chrome_download/IoT_Project/model/pose_model.pt")
+    model = YOLO("./model/pose_model.pt")
     # model = YOLO('model/pose_model.pt')
 
     # webcam
-    # cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0)
 
-    # mp4
-    cap = cv2.VideoCapture(
-        "/Users/sangwon_back/Chrome_download/IoT_Project/video/falling_video.mp4"
-    )
+    # # mp4
+    # cap = cv2.VideoCapture(
+    #     "/Users/sangwon_back/Chrome_download/IoT_Project/video/falling_video.mp4"
+    # )
 
     while True:
         ret, frame = cap.read()
